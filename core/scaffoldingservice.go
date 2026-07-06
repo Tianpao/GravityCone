b2cb5f0d565723758c21a44cd397aae1773d6ccc
Tianpao <nxtianpao@163.com>
2026-07-07 00:34:08 +0800

    chore：GCCore改为GVC(GravityCone Core)[不想和GCC重名]

--- core/scaffoldingservice.go
+++ core/scaffoldingservice.go
@@ -16,7 +16,7 @@ import (
 )
 
 // BaseVendor is the default vendor suffix. Call MakeVendor to append optional prefixes.
-const BaseVendor = "GCCore v0.1.0, EasyTier v2.6.4"
+const BaseVendor = "GVC v0.1.0, EasyTier v2.6.4"
 
 func MakeVendor(prefixes ...string) string {
 	parts := make([]string, 0, len(prefixes)+1)
